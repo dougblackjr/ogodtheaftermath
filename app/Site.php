fecc8dd8a23ea51f307bf81dbe4fa3cc7d446f1c
Douglas Black <dougblackjr@gmail.com>
2018-06-17 04:33:02 -0400

    Adding Site model and controller

---
+++ app/Site.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Site extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'sites';
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    protected $fillable = [
+        'url',
+    ];
+}
